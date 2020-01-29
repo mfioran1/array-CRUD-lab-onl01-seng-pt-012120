683e5983c055658e9f8c511bc8f21ea1d4654fc4
Maurice Fiorani <mauricef97@gmail.com>
2020-01-29 21:34:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,9 @@ def add_element_to_start_of_array(array, element)
   end
 
 def remove_element_from_end_of_array(array)
-  
-end
+   dogs = ["Max", "Buster", "Cody", "Sam", "arrays!"]
+   array = dogs.pop
+  end
 
 def remove_element_from_start_of_array(array)
   
