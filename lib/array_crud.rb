446130d0fa59d8f490df5b19894812abd30aa932
Maurice Fiorani <mauricef97@gmail.com>
2020-01-29 21:31:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   dogs = ["Max", "Buster", "Cody", "Sam"]
-  dogs.unshift("Scott")
+  dogs.unshift("wow")
   end
 
 def remove_element_from_end_of_array(array)
